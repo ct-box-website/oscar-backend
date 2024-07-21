6518d44f1419d9dfb34f8e069bcc8ee6b878935d
Chenter <chenterphai61@gmail.com>
2024-07-21 14:38:59 +0700

    readByID.php Successfully

---
+++ _backend/api/user/readByID.php
@@ -0,0 +1,49 @@
+<?php
+
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+error_reporting(0);
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Method: POST");
+header("Content-Type: application/json");
+
+include_once "../../config/init.php";
+
+$user = new User($db);
+// $user->id = isset($_POST["id"]) ?? die();
+
+if (isset($_POST["id"])) {
+    switch (($_POST["id"])) {
+        case "":
+            $erro = $user->error404("User Not Found");
+            echo $erro;
+            break;
+        default:
+            $user->id = $_POST["id"];
+            $user->readById();
+            $users = [
+                "id" => $user->id,
+                "username" => $user->username,
+                "password" => $user->password,
+                "email" => $user->email,
+                "address" => $user->address,
+            ];
+
+            $data = [
+                'code' => 1,
+                'status' => 200,
+                'msg' => 'Get single user successfully!',
+                'data' => $users
+            ];
+
+            header("HTTP/1.0 200 OK");
+            echo json_encode($data, JSON_PRETTY_PRINT);
+            break;
+    }
+} else {
+    $erro = $user->error404("User Not Found");
+    echo $erro;
+}
+
+?>
