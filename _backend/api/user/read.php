68e589fb1edaf56d39619ef9fbddbe27a05e7264
Chenter <chenterphai61@gmail.com>
2024-07-21 14:23:25 +0700

    read.php Successfully

---
+++ _backend/api/user/read.php
@@ -0,0 +1,48 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+include_once "../../config/init.php";
+
+$user = new User($db);
+$result = $user->read();
+$num = $result->rowCount();
+
+if ($num > 0) {
+    $users = [
+        'code' => 1,
+        'status' => 200,
+        'msg' => "Get All Users Successfully!",
+    ];
+
+    $users['data'] = [];
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $data = [
+            'id' => $id,
+            'username' => $username,
+            'password' => $password,
+            'email' => $email,
+            'address' => $address
+
+        ];
+        array_push($users['data'], $data);
+    }
+    header("HTTP/1.0 200 OK");
+    echo json_encode($users, JSON_PRETTY_PRINT);
+} else {
+    $users = [
+        'code' => 0,
+        'status' => 404,
+        'msg' => "Get All Users Unsuccessfully!",
+        'data' => null
+    ];
+    header("HTTP/1.0 404 User Not Found");
+    echo json_encode($users, JSON_PRETTY_PRINT);
+}
+
+?>
